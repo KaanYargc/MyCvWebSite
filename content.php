66cf11ab584c27a07ec18680d72af0d1c49488ab
Kaan Yargıcı <51912619+KaanYargc@users.noreply.github.com>
2024-02-04 11:51:16 +0200

    super bro

--- content.php
+++ content.php
@@ -1,59 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-
-    <link href="https://fonts.googleapis.com/css2?family=Lemon&display=swap" rel="stylesheet">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <title>Kaan Yargc - A Carrier</title>
-    <meta charset="UTF-8">
-    <meta name="description"
-        content="Imbued with the relentless drive of Kaan Yargıcı, I aim to transcend the conventional boundaries of the financial and software domains. My mission is to architect a future where cutting-edge financial technologies and software innovations converge harmoniously. Fueled by an unyielding passion for programming and a dedication to precision, I aim to spearhead transformative projects at the intersection of finance and software development.">
-    <meta name="keywords" content="Kaan, Yargıcı, Kaan Yargıc, Yargc, Kaan Yargc">
-    <meta name="author" content="Kaan Yargıcı">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <style>
-        body {
-            background-color: #100c0c;
-
-            .skills {
-                background-color: #1f0d0d;
-            }
-
-            .contact {
-                background-color: #1f0d0d;
-            }
-
-            .content {
-                background-color: #1f0d0d;
-            }
-
-        }
-    </style>
-
-</head>
-
-<body class=" flex items-center justify-center text-center">
-    <div class="text-center   ">
-        <div class="hover:bg-gradient-to-t w-fit h-auto rounded-3xl ">
-            <div
-                class="  transition duration-300 transform hover:scale-110 text-7xl bg-gradient-to-tr from-yellow-600 via-orange-500 to-red-600 hover:bg-gradient-to-tr hover:from-yellow-500 hover:via-orange-300 hover:to-red-600 text-transparent bg-clip-text leading-normal font-extrabold pb-4">
-                Kaan Yargc
-                <div class="text-slate-300 clear-both font-bold text-lg"><strike>Fucking</strike> Stack Developer</div>
-            </div>
             <div class="  w-11/12  content mx-auto rounded-3xl  border-dashed border-2 border-orange-950 "  >
                 <div
                     class="   transition opacity-80 duration-300 transform hover:scale-110 text-5xl bg-gradient-to-tr from-yellow-500 via-orange-400 to-red-500 hover:bg-gradient-to-tr hover:from-yellow-500 hover:via-orange-300 hover:to-red-600 text-transparent bg-clip-text leading-normal font-extrabold">
@@ -127,11 +72,9 @@
                         Contact
                     </div>
                     <div class="flex gap-2 justify-center">
-                        <div class="mb-4 flex items-center gap-2 ">
+                        <div class="mb-4 flex items-center gap-2">
 
-                            
-
-                            <a href="mailto:PasificGamer@proton.me" target="_blank">
+                            <a href="mailto:PasificGamer@proton.me" target="_blank" aria-label="Read more about Seminole tax hike">
                                 <div
                                     class="transition duration-300 transform hover:scale-110 flex gap-1 px-5 bg-opacity-85 bg-gradient-to-tr from-red-800 via-blue-400 to-yellow-800 rounded-full w-fit">
                                     <span class="leading-normal text-slate-100 material-symbols-outlined">
@@ -143,7 +86,7 @@
                     </div>
                     </a>
 
-                    <a href="https://discordapp.com/users/642643798522068992" target="_blank">
+                    <a href="https://discordapp.com/users/642643798522068992" target="_blank" aria-label="Read more about Seminole tax hike">
                         <div
                             class="transition duration-300 transform hover:scale-110 flex gap-1 px-5 bg-opacity-85 bg-gradient-to-tr from-red-800 via-blue-400 to-yellow-800 rounded-full w-fit">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="36" fill="white"
@@ -155,7 +98,7 @@
                         </div>
                     </a>
 
-                    <a href="https://t.me/Kraken1747" target="_blank">
+                    <a href="https://t.me/Kraken1747" target="_blank" aria-label="Read more about Seminole tax hike">
                         <div
                             class="transition duration-300 transform hover:scale-110 flex gap-1 px-5 bg-opacity-85 bg-gradient-to-tr from-red-800 via-blue-400 to-yellow-800 rounded-full w-fit">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="36" fill="white"
@@ -178,13 +121,3 @@
         </div>
     </div>
 
-
-    <img width="full" height="auto" src="pat.png" class="mx-auto my-auto" alt="footer">
-    </div>
-    </div>
-</body>
-
-
-
-
-</html>
