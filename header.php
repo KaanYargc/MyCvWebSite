d071cfceaaea7de4cdadf1139045c78001b848f5
Kaan Yargıcı <51912619+KaanYargc@users.noreply.github.com>
2024-02-04 11:58:48 +0200

    Create header.php

---
+++ header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link href="https://fonts.googleapis.com/css2?family=Lemon&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>Kaan Yargc - A Carrier</title>
+    <meta charset="UTF-8">
+    <meta name="description"
+        content="Imbued with the relentless drive of Kaan Yargıcı, I aim to transcend the conventional boundaries of the financial and software domains. My mission is to architect a future where cutting-edge financial technologies and software innovations converge harmoniously. Fueled by an unyielding passion for programming and a dedication to precision, I aim to spearhead transformative projects at the intersection of finance and software development.">
+    <meta name="keywords" content="Kaan, Yargıcı, Kaan Yargıc, Yargc, Kaan Yargc">
+    <meta name="author" content="Kaan Yargıcı">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color">
+
+    <style>
+        body {
+            background-color: #100c0c;
+
+            .skills {
+                background-color: #1f0d0d;
+            }
+
+            .contact {
+                background-color: #1f0d0d;
+            }
+
+            .content {
+                background-color: #1f0d0d;
+            }
+
+        }
+    </style>
+
+</head>
+
+<body class=" flex items-center justify-center text-center">
+    <div class="text-center   ">
+        <div class="hover:bg-gradient-to-t w-fit h-auto rounded-3xl ">
+            <div
+                class="  transition duration-300 transform hover:scale-110 text-7xl bg-gradient-to-tr from-yellow-600 via-orange-500 to-red-600 hover:bg-gradient-to-tr hover:from-yellow-500 hover:via-orange-300 hover:to-red-600 text-transparent bg-clip-text leading-normal font-extrabold pb-4">
+                Kaan Yargc
+                <div class="text-slate-300 clear-both font-bold text-lg"><strike>Fucking</strike> Stack Developer</div>
+            </div>
